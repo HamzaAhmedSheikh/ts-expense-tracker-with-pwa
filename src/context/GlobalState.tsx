994f7bf0e00052d87b1360e988b2b7ef07f43129
HamzaAhmedSheikh <hamzaahmedsheikh313@gmail.com>
2020-09-08 02:00:35 +0500

    create global state

--- src/context/GlobalState.tsx
+++ src/context/GlobalState.tsx
@@ -8,44 +8,45 @@ import { TransactionType, initialStateType } from '../Types/ExpenseTrackerTypes'
 
  // create global state
 
- const initialstate: initialStateType = {
-     Transaction: [{ id: 1, text: "Cash", amount: 200 }],
-     deleteTransaction: () => {},
-     addTransaction: () => {},
- }
+   const initialstate: initialStateType = {
+       Transaction: [{ id: 1, text: "Cash", amount: 200}],
+       deleteTransaction: () => {},
+       addTransaction: () => {},
+   } 
 
- export const TransactionContext = createContext(initialstate);
+
+//  export const TransactionContext = createContext(initialstate);
  
- export const TransactionProvider: React.FC = ({ children }) => {
+//  export const TransactionProvider: React.FC = ({ children }) => {
 
-    const [state, dispatch] = useReducer(Reducer, initialstate)
+//     const [state, dispatch] = useReducer(Reducer, initialstate)
 
      //Actions
     //Actions types is handiling in Reducer
 
-    function addTransaction(transactionData: TransactionType) {
-        dispatch({
-            type: "Add_Transaction",
-            payload: transactionData,
-        });        
-    }
+//     function addTransaction(transactionData: TransactionType) {
+//         dispatch({
+//             type: "Add_Transaction",
+//             payload: transactionData,
+//         });        
+//     }
 
-    function deleteTransaction(id: number) {
-        dispatch({
-            type: "Delete_Transaction",
-            payload: id,
-        });
-    }
+//     function deleteTransaction(id: number) {
+//         dispatch({
+//             type: "Delete_Transaction",
+//             payload: id,
+//         });
+//     }
 
-    return (
-      <div>
-         <TransactionContext.Provider value={{
-            Transaction: state.Transaction,
-            deleteTransaction,
-            addTransaction,
-         }}>
-            {children}
-         </TransactionContext.Provider>  
-      </div>
-    )
- }
+//     return (
+//       <div>
+//          <TransactionContext.Provider value={{
+//             Transaction: state.Transaction,
+//             deleteTransaction,
+//             addTransaction,
+//          }}>
+//             {children}
+//          </TransactionContext.Provider>  
+//       </div>
+//     )
+//  }
